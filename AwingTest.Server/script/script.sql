2275316b4e11d58797dbeb1da5d7e505daaef898
LamTran7801 <147394897+LamTran7801@users.noreply.github.com>
2025-09-11 00:21:30 +0700

    Add db script

---
+++ AwingTest.Server/script/script.sql
@@ -0,0 +1,11 @@
+﻿drop table if exists treasure_map;
+create table treasure_map(
+	id serial primary key,
+	rows int4,
+	columns int4,
+	chest_types int4,
+	min_total_distance double precision,
+	map_offset_str text,
+	path_x_offset_str text,
+	path_y_offset_str text
+);
